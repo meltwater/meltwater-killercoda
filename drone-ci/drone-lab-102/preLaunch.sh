9dd90ab1403cfed3db5d03a138e065f1a9f9fa93
kenny Adetutu <kenny.adetutu@meltwater.com>
2022-06-07 14:45:42 -0400

    changed index.json layout

--- drone-ci/drone-lab-102/preLaunch.sh
+++ drone-ci/drone-lab-102/preLaunch.sh
@@ -1,4 +1,5 @@
-ssh root@host01 "curl -L https://github.com/harness/drone-cli/releases/download/v1.5.0/drone_linux_amd64.tar.gz | tar zx && sudo install -t /usr/local/bin drone"
-ssh root@host01 "cd /class && docker-compose up -d"
-ssh root@host01 "docker pull philipharries/html_tidy"
-ssh root@host01 "docker pull docker"
+#!/bin/bash
+curl -L https://github.com/harness/drone-cli/releases/download/v1.5.0/drone_linux_amd64.tar.gz | tar zx && sudo install -t /usr/local/bin drone
+cd /class && docker-compose up -d
+docker pull philipharries/html_tidy
+docker pull docker
