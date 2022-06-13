4744a4836904e6b2343f26f5271881dba0cef560
kenny Adetutu <kenny.adetutu@meltwater.com>
2022-06-13 13:57:19 -0400

    ATEAM-12673: Updated preLaunch.sh

--- elastalert/elastalert-lab-101/preLaunch.sh
+++ elastalert/elastalert-lab-101/preLaunch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo apt update && sudo apt install python3-pip
-pip3 install elasticsearch==5.2.0 elastalert==0.1.36
+touch ~/my_sample.txt
+apt update -y && apt install python3-pip -y
+#pip3 install elasticsearch==5.2.0 elastalert==0.1.36
