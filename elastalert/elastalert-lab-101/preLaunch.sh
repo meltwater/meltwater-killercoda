d2b72100237368182e994a407aec9ba50f3e86e8
Brian Burnett <hikerspath@users.noreply.github.com>
2022-06-14 14:07:25 -0400

    Update preLaunch.sh

--- elastalert/elastalert-lab-101/preLaunch.sh
+++ elastalert/elastalert-lab-101/preLaunch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-apt install python2-pip -y
+# Download Ancient Code
+apt install python2-dev -y
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 python2 get-pip.py
 rm get-pip.py
+# Install Ancient Libraries
 pip2 install elasticsearch==5.2.0 >> ~/error1.log
 pip2 install elastalert==0.1.35 >> ~/error1.log
+# Replace Current Runtime with Ancient
+ln -sf /usr/bin/python2 /etc/alternatives/python
