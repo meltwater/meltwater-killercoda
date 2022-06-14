85520df18132592982a125aae25d3f8160ff1ea6
Brian Burnett <hikerspath@users.noreply.github.com>
2022-06-14 13:59:04 -0400

    Update preLaunch.sh

--- elastalert/elastalert-lab-101/preLaunch.sh
+++ elastalert/elastalert-lab-101/preLaunch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-apt update -y && apt install python3-pip -y
-pip3 install elasticsearch==5.2.0 >> ~/error1.log
-pip3 install elastalert >> ~/error1.log
+apt install python2-pip -y
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+python2 get-pip.py
+rm get-pip.py
+pip2 install elasticsearch==5.2.0 >> ~/error1.log
+pip2 install elastalert==0.1.35 >> ~/error1.log
