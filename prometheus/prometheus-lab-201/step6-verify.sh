3299d1a71a31ab8927a24766f01eea29caf444fa
Brian Burnett <hikerspath@users.noreply.github.com>
2022-06-15 14:03:40 -0400

    ATEAM-12669: Final Attempt At Verification (#65)

--- prometheus/prometheus-lab-201/step6-verify.sh
+++ prometheus/prometheus-lab-201/step6-verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-if [[ $(curl -s localhost:30090/api/v1/query -d 'query=probe_success{}' | jq '.data.result | length') -eq 2 ]]; then 
+if [ "$(curl -s localhost:30090/api/v1/query -d 'query=probe_success{}' | jq '.data.result | length')" == "2" ]; then 
   echo done; 
 else 
   echo failed && exit 1; 
