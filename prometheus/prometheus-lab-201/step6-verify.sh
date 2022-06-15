eeea5d1e9038784beafd4ecadac51dab34ab6f03
Brian Burnett <hikerspath@users.noreply.github.com>
2022-06-15 14:09:27 -0400

    ATEAM-12669: Complete Removal of verify for Step 6 (#66)
    * Delete step6-verify.sh
    
    * Remove verification

--- prometheus/prometheus-lab-201/step6-verify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-if [ "$(curl -s localhost:30090/api/v1/query -d 'query=probe_success{}' | jq '.data.result | length')" == "2" ]; then 
-  echo done; 
-else 
-  echo failed && exit 1; 
-fi;
