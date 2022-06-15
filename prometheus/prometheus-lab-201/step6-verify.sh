be8976b5353822d8cacb85175059b2cdbf67b5d8
Brian Burnett <hikerspath@users.noreply.github.com>
2022-06-15 11:54:33 -0400

    ATEAM-12669: Prometheus 201 Final (#61)

--- prometheus/prometheus-lab-201/step6-verify.sh
+++ prometheus/prometheus-lab-201/step6-verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-if [ $(kubectl get pods | grep 'blackbox\|prometheus' | grep Running | wc -l) == 2 ]; then 
+if [[ $(kubectl get pods | grep 'blackbox\|prometheus' | grep Running | wc -l) == 2 ]]; then 
   echo done; 
 else 
   echo failed && exit 1; 
