87d18abe9722a92e14b37b5f350b11319765ea4c
Brian Burnett <hikerspath@users.noreply.github.com>
2022-06-15 11:27:28 -0400

    ATEAM-12669: Prometheus 201 Fixes (#60)

--- prometheus/prometheus-lab-201/step1-foreground.sh
+++ prometheus/prometheus-lab-201/step1-foreground.sh
@@ -1,5 +1,5 @@
 echo -n "Waiting for prometheus." && \
-while $(kubectl get pods | grep prometheus | grep -qv Running); then
+while $(kubectl get pods | grep prometheus | grep -qv Running); do
   echo -n ".";
   sleep 1;
 done && echo -e "\n\n" && \
